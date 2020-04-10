b08256a03cf750efec3df7e3aaa988a6c19eb006
András Szucsák <andras.szucsak@urss.hu>
2020-04-10 23:29:07 +0200

    set up auth guards, create training service

---
+++ src/app/training/training.service.ts
@@ -0,0 +1,22 @@
+import { Exercise } from './exercise.model';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class TrainingService {
+  private availableExercises: Exercise[] = [
+    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
+    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
+    { id: 'burpees', name: 'Burpees', duration: 80, calories: 8 },
+  ];
+
+  private runningExercise: Exercise;
+
+  getAvailableExercises() {
+    return this.availableExercises.slice();
+  }
+
+  startExercise(selectedId: string) {
+    this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
+  }
+}
